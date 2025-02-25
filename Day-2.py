d5939f1d6676cad881aeec025faeb5e68d32ba47
craftcodenoman <161233856+craftcodenoman@users.noreply.github.com>
2025-02-25 15:47:19 +0500

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,12 @@
+user_sentence = input("Enter your sentence: ")
+print(f"Hello  {user_sentence} Your Word Count is {len(user_sentence)} ")
+print(f"Hello  {user_sentence} Your Word Count is {user_sentence.split()} ")
+
+print(f"Hello  {user_sentence} Your Word is small letter {user_sentence.lower()} ")
+print(f"Hello  {user_sentence} Your Word is capital letter {user_sentence.upper()} ")
+print(f"Hello  {user_sentence} Your Word is capitalize letter {user_sentence.capitalize()} ")
+print(f"Hello  {user_sentence} Your Word is title letter {user_sentence.title()} ")
+
+
+
+
