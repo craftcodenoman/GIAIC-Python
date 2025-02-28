751d328f429eef8507626ac7c4dd1c215fca1a1f
craftcodenoman <161233856+craftcodenoman@users.noreply.github.com>
2025-02-28 17:55:24 +0500

    Add files via upload

---
+++ Day-5.py
@@ -0,0 +1,10 @@
+#  Day 5 – Daily Python Challenge 🐍
+# 🚀 Challenge:
+# Aisa Python program likhna hai jo user se ek number lega aur uska sum calculate karega 1 se lekar us number tak. 🔢💡
+
+
+user_number = int(input("Enter Your Number: "))
+total_sum = sum(range(1, user_number + 1))
+
+print("Result is:\n")
+print(f"sum is {total_sum}")
